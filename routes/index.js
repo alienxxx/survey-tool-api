b770224bf51ba2c8e29f5f2cfaae73142ce14155
Rolf <rolf@virtualbox>
2016-12-11 22:31:07 +0100

    service refactoring

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 
 var dbPromise = MongoClient.connect('mongodb://localhost:27017/survey-tool');
 
-/* GET all questoins */
-router.get('/questions', function(req, res, next) {
+/* GET all departments */
+// TODO: select only departments for this user
+router.get('/departments', function(req, res, next) {
 	dbPromise.then( function(db) {
-	  	db.collection('questions').find().toArray(function (err, result) {
+	  	db.collection('departments').find({'members':'rhaynberg'},{'name':1}).toArray(function (err, result) {
 		    if (err) throw err 
 
 			res.json(result);
@@ -19,28 +21,48 @@ router.get('/questions', function(req, res, next) {
   	});
 });
 
-/* POST an answer */
-router.post('/answers', function(req, res, next) {
+/* GET one department */
+router.get('/departments/:id', function(req, res, next) {
+	var oid = new ObjectId(req.params.id);
+
 	dbPromise.then( function(db) {
-//		db.collection('answers').insert( {'questionId': req.params.questionId, 'answer': req.body.answer}, function (err, result) {
-		db.collection('answers').insertMany( req.body, function (err, result) {
+	  	db.collection('departments').findOne(oid,function (err, result) {
+		    if (err) throw err 
+
+			res.json(result);
+	  	})
+  	},
+  	function(err) { //promise rejected
+  		throw err
+  	});
+});
+
+/* GET all questions */
+// TODO: select only questsions for this department
+router.get('/departments/:id/questions', function(req, res, next) {
+	var oid = new ObjectId(req.params.id);
+
+	dbPromise.then( function(db) {
+	  	db.collection('questions').find({'departments':oid,'enabled':true},{'departments':false}).toArray(function (err, result) {
+		    if (err) throw err 
+
+			res.json(result);
+	  	})
+  	},
+  	function(err) { //promise rejected
+  		throw err
+  	});
+});
+
+/* UPDATE the department's answers */
+router.put('/departments/:id/answers', function(req, res, next) {
+	var oid = new ObjectId(req.params.id);
+
+	dbPromise.then( function(db) {
+		db.collection('departments').update({'_id':oid}, {'$set': {'answers': req.body}}, function (err, result) {
 	    	if (err) throw err
 
 	    	res.sendStatus(200);
-	    })
-	},
-	function(err) { //promise rejected
-		throw err
-	});
-});
-
-/* GET all answers */
-router.get('/answers', function(req, res, next) {
-	dbPromise.then( function(db) {
-		db.collection('answers').find().toArray(function (err, result) {
-	    	if (err) throw err
-
-	    	res.json(result);
 	    })
 	},
 	function(err) { //promise rejected
